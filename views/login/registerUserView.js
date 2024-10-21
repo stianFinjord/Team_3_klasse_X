abf6b75a2edf109a87aaa56b467235b15bc144e6
Northorium <63539373+Northorium@users.noreply.github.com>
2024-10-21 13:02:29 +0200

    oppdatert html

--- views/login/registerUserView.js
+++ views/login/registerUserView.js
@@ -3,11 +3,12 @@
 function renderRegisterUserView(){
     document.getElementById('app').innerHTML= /*HTML*/ `
     <div>
-        <input placeholder="E-post"><br>
-        <input placeholder="Brukernavn"><br>
-        <input placeholder="Passord"><br>
-        <input placeholder="Gjenta passord"><br>
-        <button>Registrer deg</button><br>
+    <h2>Registrer deg</h2>
+        <input type="email" class="email" placeholder="E-post"><br>
+        <input type="text" class="username" placeholder="Brukernavn"><br>
+        <input type="password" class="password" placeholder="Passord"><br>
+        <input type="password" class="repeatPassword" placeholder="Gjenta Passord"><br>
+        <button type="submit">Registrer deg</button><br>
     </div> 
     `;
 }
