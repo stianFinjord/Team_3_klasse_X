fe9200eee2e1c758da453144df3956b5717b15de
Northorium <63539373+Northorium@users.noreply.github.com>
2024-10-21 14:44:02 +0200

    changes to model

--- views/login/registerUserView.js
+++ views/login/registerUserView.js
@@ -1,15 +1,22 @@
+function renderRegisterUserView() {
+  document.getElementById("app").innerHTML = /*HTML*/ `
 
-
-function renderRegisterUserView(){
-    document.getElementById('app').innerHTML= /*HTML*/ `
     <div>
-    <h2>Registrer deg</h2>
-        <input type="email" class="input" placeholder="E-post"><br>
-        <input type="text" class="input" placeholder="Brukernavn"><br>
-        <input type="password" class="input" placeholder="Passord"><br>
-        <input type="password" class="input" placeholder="Gjenta Passord"><br>
-        <button id=registerButton>Registrer deg</button><br>
-<p> Alert </p>
+        <h2>Registrer deg</h2>
+        <input type="text" onchange="model.input.createAccount.userName = this.value" class="input" placeholder="Brukernavn"><br>
+        <input type="email" onchange="model.input.createAccount.email = this.value" class="input" placeholder="E-post"><br>
+        <input type="password" onchange="model.input.createAccount.password = this.value" class="input" placeholder="Passord"><br>
+        <input type="password" onchange="model.input.createAccount.repeatPassword = this.value" class="input" placeholder="Gjenta Passord"><br>
+        <button onchange=someFunction()  onclick=registerButton()>Registrer deg</button><br>
+ 
     </div> 
     `;
+}
+
+
+function registerButton() {
+   model.input.createAccount.userName = userName;
+   model.input.createAccount.email = email;
+   model.input.createAccount.password = password;
+   model.input.createAccount.repeatPassword = repeatPassword;
 }
