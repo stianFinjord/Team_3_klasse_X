36e2dd08c04538ed9a29c2602f6db3c6fccdd865
Stian Finjord <stian.finjord@gmail.com>
2024-10-30 09:22:07 +0100

    Resync

--- views/common/navigationMenuView.js
+++ views/common/navigationMenuView.js
@@ -1,18 +1,6 @@
-let isMenuOpen = false;
-function toggleMenuOld() {
-    if (isMenuOpen) {
-        isMenuOpen = false;
-    } else {
-        isMenuOpen = true;
-    }
-    showView();
-}
-
 function toggleMenu(button) {
     button.parentElement.classList.toggle("open");
 }
-
-
 
 function getNavigationMenu() {
     let html = /*HTML*/ `
@@ -26,28 +14,3 @@ function getNavigationMenu() {
     return html;
 }
 
-function getNavigationMenuOld() {
-    let html;
-    if (isMenuOpen) {
-        html = /*HTML*/ `
-        <div class="side-menu" style=width:140px>
-            <div class=hamburger-icon onclick=toggleMenuOld()>≡</div>
-            <button onclick=setViewRegister()>Registrer deg</button><br>
-            <button onclick=setViewRegister()>Annen view</button><br>
-            <button onclick=setViewRegister()>knapp</button><br>
-            <div>Slutt på menyen</div>
-        </div>
-    `;
-    } else {
-        html = /*HTML*/ `
-        <div class="side-menu" style=width:40px>
-            <div class=hamburger-icon onclick=toggleMenuOld()>≡</div>
-            
-        </div>
-        
-    `;
-    }
-
-
-    return html;
-}
